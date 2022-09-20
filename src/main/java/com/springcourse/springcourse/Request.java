f790f3eea783c24d1475fe79c5b4019e272cb7b8
Péricles Vasconcelos <periclesevt@gmail.com>
2022-09-19 22:51:59 -0300

    Update Request.java

--- src/main/java/com/springcourse/springcourse/Request.java
+++ src/main/java/com/springcourse/springcourse/Request.java
@@ -10,15 +10,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity(name = "request")
+
 public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 75, nullable = false)
     private String subject;
+
+    @Column(columnDefinition = "text")
     private String description;
+
+    @Column(name = "creation_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date creationDate;
+
+    @Column(length = 12, nullable = false)
+    @Enumerated(EnumType.STRING)
     private RequestState state;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "request")
     private List<RequestStage> stages = new ArrayList<RequestStage>();
 }
