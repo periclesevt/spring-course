c40b378e2bfcc99c0c18f1f53875ed53f157bb32
Péricles Vasconcelos <periclesevt@gmail.com>
2022-09-19 22:51:56 -0300

    Update RequestStage.java

--- src/main/java/com/springcourse/springcourse/RequestStage.java
+++ src/main/java/com/springcourse/springcourse/RequestStage.java
@@ -7,16 +7,35 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity(name = "request_stage")
 public class RequestStage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Date realizationDate;
+
+    @Column(columnDefinition = "text")
     private String description;
+
+    @Column(name = "realization_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date realizationDate;
+
+    @Column(length = 12, nullable = false)
+    @Enumerated(EnumType.STRING)
     private RequestState state;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id", nullable = false)
     private Request request;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
 }
