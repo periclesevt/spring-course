0849670a4989573fa99a1ca1a504f2b67dc2c2fb
Péricles Vasconcelos <periclesevt@gmail.com>
2022-09-19 22:51:54 -0300

    Update User.java

--- src/main/java/com/springcourse/springcourse/User.java
+++ src/main/java/com/springcourse/springcourse/User.java
@@ -1,24 +1,41 @@
 package com.springcourse.springcourse;
 
 import com.springcourse.springcourse.domain.enums.Role;
-import jdk.jfr.internal.RequestEngine;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 75, nullable = false)
     private String name;
+
+    @Column(length = 75, nullable = false, unique = true)
     private String email;
+
+    @Column(length = 100, nullable = false)
     private String password;
+
+    @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToMany(mappedBy = "user")
     private List<Request> requests = new ArrayList<Request>();
+
+    @OneToMany(mappedBy = "user")
     private List<RequestStage> stages = new ArrayList<RequestStage>();
 }
