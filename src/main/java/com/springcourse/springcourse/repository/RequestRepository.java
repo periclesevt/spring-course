ee55965b50c167cc3913aec304d68121691f11bd
Péricles Vasconcelos <periclesevt@gmail.com>
2022-09-29 17:06:21 -0300

    Create RequestRepository.java

---
+++ src/main/java/com/springcourse/springcourse/repository/RequestRepository.java
@@ -0,0 +1,14 @@
+package com.springcourse.springcourse.repository;
+
+import com.springcourse.springcourse.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.function.LongConsumer;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    public List<Request> findAllOwnerId(Long id);
+}
